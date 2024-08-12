c689e9f75bbfd0e9a5810d0f717836d5bbf8b627
Manikandan R S <129031397+ManileeDev@users.noreply.github.com>
2024-08-12 09:23:19 +0530

    Delete src/utils directory

--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-const YT_API_KEY = "AIzaSyCFMtq8CJ5jCYSOh6k804rs6Y3PNaYXLxI"
-export const YT_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${YT_API_KEY}`
